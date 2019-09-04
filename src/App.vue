5cd25d5b77b596e8e81529bb0c53dc4234d845ce
salazenas <salazenas@gmail.com>
2019-09-04 13:40:47 -0300

    project started

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <i class="fas fa-coffee"></i>
+    <font-awesome-icon icon="user-secret" />
+    <h1>{{ message }}</h1>
+    <input/>
+  </div>
+</template>
+
+<script>
+import Input from "./components/Input";
+
+export default {
+  data: () => ({
+    message: "GitHub Manager"
+  }),
+  components: {
+    Input
+  }
+};
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <label for="basic-url">Your vanity URL</label>
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span
+          class="input-group-text"
+          id="basic-addon3">
+         </span>
+      </div>
+      <input
+        type="text"
+        class="form-control"
+        id="basic-url"
+        aria-describedby="basic-addon3"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
